a26c17a7afe2423932c72cc545a495131e66ea3b
Shyam Panchal <snpanchal44@gmail.com>
2019-09-10 00:24:40 -0400

    Add url manager class

---
+++ app/src/main/java/com/shyampanchal/lendify/utils/UrlManager.kt
@@ -0,0 +1,9 @@
+package com.shyampanchal.lendify.utils
+
+object UrlManager {
+
+    const val projectKey = "lendify-44e2a"
+    const val baseUrl = "https://$projectKey.firebaseio.com/"
+
+    const val itemsUrl = "items.json"
+}
