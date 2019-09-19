ecf03d5f71765a625f13ad2bbee567faad7b520c
Shyam Panchal <snpanchal44@gmail.com>
2019-09-19 02:34:32 -0400

    Show toast message for error

--- app/src/main/java/com/shyampanchal/lendify/ui/HomeActivity.kt
+++ app/src/main/java/com/shyampanchal/lendify/ui/HomeActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -49,6 +50,10 @@ class HomeActivity : AppCompatActivity() {
                         }
                 }
             }
+        })
+
+        vm.errorMessage.observe(this, Observer {
+            Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
         })
     }
 
